224e78d843a971d88fbc5e51f4c15c63ef212545
Naiem Hasan <naiemjoy1@gmail.com>
2024-09-09 14:21:17 +0600

    banner in doctor update

--- src/Components/Hooks/useAxiosSecure.jsx
+++ src/Components/Hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import useAuth from "./useAuth";
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://care-quest-server.vercel.app//",
 });
 
 const useAxiosSecure = () => {
